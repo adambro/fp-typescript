b24bc9cb8884af2bf9dacec9bfc5d30a83e277cf
Rafał Brzoska <rafal@brzoska.net.pl>
2017-12-19 06:56:28 +0100

    init

---
+++ app/app.ts
@@ -0,0 +1,10 @@
+import { randomNumber } from "./utils";
+
+randomNumber();
+class App {
+    private id = 'init';
+
+    tableLog(msg: object) {
+        console.table(msg)
+    }
+}
---
+++ app/webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    entry: "./app.ts",
+    output: {
+        filename: "bundle.js"
+    },
+    resolve: {
+        extensions: [".ts"]
+    },
+    module: {
+        loaders: [
+            { test: /\.ts?$/, loader: "ts-loader" }
+        ]
+    }
+}
