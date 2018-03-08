ae783beabd4079be35cb8b324c3c4de2abac4745
Rafał Brzoska <rafal@brzoska.net.pl>
2018-03-08 09:46:35 +0100

    class

--- dist/es6.js
+++ dist/es6.js
@@ -1,6 +1,14 @@
 "use strict";
 //console.log(a);
 //console.log(b);
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
 var a = 5;
 var b = 6;
 var c = {
@@ -38,4 +46,15 @@ function myFn(p1) {
 }
 myFn(1, 2, 3, 4, 5, 6, "test");
 var arr = [1, 2, 3];
+var newArray = arr.concat(arr);
 console.log.apply(console, arr);
+var testobj = {
+    a: 1,
+    b: 2
+};
+var testobj2 = __assign({}, testobj);
+var str1 = "Hello";
+var str2 = "World";
+console.log(str1 + " "
+    + str2);
+console.log(str1 + " \n\n" + str2);
--- index.ts
+++ index.ts
@@ -1,1 +1,33 @@
+let num = 1;
+let str = 'test'
+let bool = false;
+let numArr = [1,2,3,4];
 
+num = 1;
+numArr.push(num);
+
+function testfn(num: number):never {
+    throw new Error();
+}
+
+class User {
+    private _name: string;
+    constructor(newName: string) {
+        this._name = newName;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(newName: string) {
+        this._name = newName;
+    }
+
+    getHelloName(): string {
+        return 'hello '+ this.name;
+    }
+}
+
+let user = new User('Alojzy');
+console.log(user.getHelloName);
